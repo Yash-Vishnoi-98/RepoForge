b2a305901c15727a1beb2270682ee12b48399343
Yash Vishnoi <yashvishnoi01.yv@gmail.com>
2025-07-12 12:30:39 +0530

    add uRL

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -2,7 +2,7 @@ import passport from "passport";
 import dotenv from "dotenv";
 import { Strategy as GitHubStrategy } from "passport-github2";
 import User from "../models/user.model.js";
- 
+
 dotenv.config();
 
 // Passport session setup.
@@ -14,13 +14,12 @@ dotenv.config();
 //   and deserialized.
 
 passport.serializeUser(function (user, done) {
-	done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function (obj, done) {
-	done(null, obj);
+  done(null, obj);
 });
-
 
 // Use the GitHubStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
@@ -28,30 +27,30 @@ passport.deserializeUser(function (obj, done) {
 //   profile), and invoke a callback with a user object.
 
 passport.use(
-	new GitHubStrategy(
-		{
-			clientID: process.env.GITHUB_CLIENT_ID,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "http://localhost:3000/api/auth/github/callback",
-			// callbackURL: "https://mern-github-app.onrender.com/api/auth/github/callback",
-		},
-		async function (accessToken, refreshToken, profile, done) {
-			const user = await User.findOne({ username: profile.username });
-			// signup
-			if (!user) {
-				const newUser = new User({
-					name: profile.displayName,
-					username: profile.username,
-					profileUrl: profile.profileUrl,
-					avatarUrl: profile.photos[0].value,
-					likedProfiles: [],
-					likedBy: [],
-				});
-				await newUser.save();
-				done(null, newUser);
-			} else {
-				done(null, user);
-			}
-		}
-	)
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      // callbackURL: "http://localhost:3000/api/auth/github/callback",
+      callbackURL: "https://repoforge.onrender.com/api/auth/github/callback",
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const user = await User.findOne({ username: profile.username });
+      // signup
+      if (!user) {
+        const newUser = new User({
+          name: profile.displayName,
+          username: profile.username,
+          profileUrl: profile.profileUrl,
+          avatarUrl: profile.photos[0].value,
+          likedProfiles: [],
+          likedBy: [],
+        });
+        await newUser.save();
+        done(null, newUser);
+      } else {
+        done(null, user);
+      }
+    }
+  )
 );
